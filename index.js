3277358097bd3a6785e419faed337d302e362d2b
Alena <i.am.am.629@gmail.com>
2018-07-31 20:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  var new = kittens.concat(name)
+  return new
+}
+
+function prependKitten(name){
+  
+}
+
+function removeLastKitten(){
+  
+}
+
+function removeFirstKitten(){
+  
+}
